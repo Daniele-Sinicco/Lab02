427345a64abcf5f530c62bbb9af9d6a31bdeabb2
Daniele-Sinicco <s268448@studenti.polito.it>
2024-04-16 17:59:17 +0200

    Lab 05 eseguito

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,30 @@
 class Dictionary:
-    def __init__(self):
-        pass
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
 
-    def addWord(self):
-        pass
+    def addWord(self, word):
+        self.dictionary.append(word.lower())
 
-    def translate(self):
-        pass
+    def translate(self, aliena):
+        trovata = False
+        for w in self.dictionary:
+            if aliena.lower() in w.lower():
+                trovata = True
+                riga_split = w.split(" ")
+                if len(riga_split) == 2:
+                    return riga_split[1]+"\n"
+                elif len(riga_split) > 2:
+                    traduzione = ""
+                    primo = False
+                    for i in range(1, len(riga_split)):
+                        if primo is False:
+                            traduzione += riga_split[i]
+                            primo = True
+                        else:
+                            traduzione += " "+riga_split[i]
+                    return traduzione
+        if trovata == False:
+            return "Parola non trovata\n"
 
     def translateWordWildCard(self):
         pass
--- main.py
+++ main.py
@@ -7,19 +7,50 @@ while(True):
 
     t.printMenu()
 
-    t.loadDictionary("filename.txt")
+    t.loadDictionary("dictionary.txt")
 
-    txtIn = input()
+    opzione = input("Seleziona un'opzione: ")
 
     # Add input control here!
 
-    if int(txtIn) == 1:
-        print()
-        txtIn = input()
-        pass
-    if int(txtIn) == 2:
-        pass
-    if int(txtIn) == 3:
-        pass
-    if int(txtIn) == 4:
+    if int(opzione) == 1:
+        txtIn = input("Ok, quale parola devo aggiungere? ")
+        t.handleAdd(txtIn)
+        print("Aggiunta!\n")
+    if int(opzione) == 2:
+        txtIn = input("Ok, quale parola devo cercare? ")
+        """input_valido = True
+        for char in txtIn:
+            if not (65 < ord(char) < 90 or 97 < ord(char) < 122):
+                input_valido = False
+                print("Inserimento non valido, per parole con caratteri speciali selezionare l'opzione 3.\n")
+                break
+        if input_valido is True:
+            print(t.handleTranslate(txtIn))"""
+        if txtIn.isalpha():
+            print(t.handleTranslate(txtIn))
+        else:
+            print("Inserimento non valido, per parole con caratteri speciali selezionare l'opzione 3.\n")
+
+    if int(opzione) == 3:
+        txtIn = input("Ok, quale parola devo cercare? ")
+        pos_da_sostituire = 0
+        conta_speciali = 0
+        wild_card = ""
+        for char in txtIn:
+            if not (('a' <= char <= 'z' or 'A' <= char <= 'Z') or char == '?'):
+                conta_speciali += 1
+                pos_da_sostituire = txtIn.index(char)
+        if conta_speciali == 0:
+            print(t.handleTranslate(txtIn))
+        elif conta_speciali == 1:
+            wild_card = txtIn[:pos_da_sostituire]+"?"+txtIn[pos_da_sostituire+1:]
+            #print(t.handleWildCard(wild_card))
+        else:
+            print("Inserimento non valido, è ammesso un solo carattere speciale per parola,"
+                  " seleziona nuovamente l'opzione 3 con inserimento corretto.\n")
+
+        print(wild_card)
+
+    if int(opzione) == 4:
         break
